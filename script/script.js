6793031ccf6584678c9c606bb19f813c96da11e4
Abusayedshuvo <abusayedshuvo24@gmail.com>
2024-10-11 08:48:15 +0600

    Phone API with javascript

---
+++ script/script.js
@@ -0,0 +1,17 @@
+const loadPhone = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/phones?search=iphone"
+  );
+  const data = await res.json();
+  const phones = data.data;
+  displayPhone(phones);
+};
+
+const displayPhone = (phones) => {
+  const phoneContainer = document.getElementById("allPhones");
+  phones.forEach((phone) => {
+    console.log(phone);
+  });
+};
+
+loadPhone();
